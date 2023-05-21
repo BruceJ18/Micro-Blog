d4601f088ba813efd5303410f747330450e861fb
Bruce Jacobo <bruceja18@protonmail.com>
2023-05-20 17:57:21 -0700

    Remove unecessary test code.

--- app.py
+++ app.py
@@ -6,8 +6,4 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        entry_content = request.form.get("content")
-        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        print(entry_content, formatted_date)
     return render_template("index.html")
