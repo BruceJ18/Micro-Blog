8621b28814f3a482fed7fd67ff26b75fb6cdc844
Bruce Jacobo <bruceja18@protonmail.com>
2023-05-21 11:42:21 -0700

    Retrieve data from MongoDB.

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
 from flask import Flask, render_template, request
 import datetime
+from pymongo import MongoClient
+
+
 
 app = Flask(__name__)
+
+db_client = MongoClient('mongodb+srv://brucej18:illescio369@cluster0.5bjmfpv.mongodb.net/')
+app.db = db_client.MicroBlog
+
+
+
+
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+    entries_with_date = [(
+        entry["content"], 
+        entry["date"], 
+        datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in app.db.entries.find({})
+    ]
+    return render_template("index.html", entries=entries_with_date)
